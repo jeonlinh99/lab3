3e828d1573a225215f31c7785e1fc219c2d19cee
Quan <manhquan24072000@gmail.com>
2021-06-14 15:31:13 +0700

    add footer code

---
+++ index.php
@@ -0,0 +1,126 @@
+<?php 
+?>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Web</title>
+    
+        <!-- Bootstrap4 - CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+            integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous" />
+    
+        <!-- Font-awesome -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+            integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w=="
+            crossorigin="anonymous" />
+    
+        <!-- Main - CSS  -->
+        <link rel="stylesheet" href="./css/main.css" />
+    
+    </head>
+    <body>
+        <!-- HEADER -->
+        <header></header>
+        <!-- MAIN -->
+        <section>
+    
+        </section>
+        <!-- FOOTER -->
+        <footer>
+            <div class="footer__top section">
+                <div class="container">
+                    <div class="footer__grid row">
+                        <div class="col-lg-4 col-12">
+                            <h3>About Us</h3>
+                            <p>Nam libero tempore cum vulputate id est id, pretium semper enim. Morbi viverra congue nisi vel
+                                pulvinar posuere sapien eros.</p>
+                        </div>
+                        <div class="col-lg-4 col-12">
+                            <h3>Latest News</h3>
+                            <div class="footer__link">
+                                <i class="fa fa-long-arrow-alt-right"></i>
+                                <a href="">Lorem ipsum neque vulputate</a>
+                            </div>
+                            <div class="footer__link">
+                                <i class="fa fa-long-arrow-alt-right"></i>
+                                <a href="">Dolor amet sed quam vitae</a>
+                            </div>
+                            <div class="footer__link">
+                                <i class="fa fa-long-arrow-alt-right"></i>
+                                <a href="">Lorem ipsum neque vulputate</a>
+                            </div>
+                            <div class="footer__link">
+                                <i class="fa fa-long-arrow-alt-right"></i>
+                                <a href="">Dolor amet sed quam vitae</a>
+                            </div>
+                            <div class="footer__link">
+                                <i class="fa fa-long-arrow-alt-right"></i>
+                                <a href="">Lorem ipsum neque vulputate</a>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-12">
+                            <h3>Contact Info</h3>
+                            <div class="footer__contact">
+                                <i class="fa fa-map-marker-alt"></i>
+                                <a href="">Ha Noi, Viet Nam</a>
+                            </div>
+                            <div class="footer__contact">
+                                <i class="far fa-envelope"></i>
+                                <a href="mailto: manhquan.thd@gmail.com"> manhquan.thd@gmail.com</a>
+                            </div>
+                            <div class="footer__contact">
+                                <i class="fas fa-phone-alt"></i>
+                                <a href="tel:0382407247"> 038.240.7247</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="footer__form">
+                        <form>
+                            <div class="form-group  form-row ">
+                                <label class="col-lg-4 col-12  col-form-label" for="staticEmail2">Sign up for our Newsletter</label>
+                                <div class="col-lg-6 col-8">
+                                    <input  type="email" class="form-control" id="staticEmail2" placeholder="email@example.com">
+                                </div>
+                                <div class="col-lg-2 col-4 text-center">
+                                    <button type="submit" class="btn btn-primary">Submit</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <div class="footer__bottom">
+                <div>
+                    © 2020 Instruction. All rights reserved | Design by <a href="#">Me</a>
+                </div>
+            </div>
+        </footer>
+    
+    
+    
+    
+    
+    
+    
+        <!-- Jquery - 3.6 -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"
+            integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ=="
+            crossorigin="anonymous">
+            </script>
+    
+        <!-- Bootstrap 4 - JS -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+            integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+            </script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+            integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+            </script>
+    
+        <!-- Main JS -->
+        <script src="./js/main.js"></script>
+    
+    </body>
+    </html>
